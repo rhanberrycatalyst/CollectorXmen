7cff92a37e5beab30a42c8259038549c9f5b7b68
Rick Hanberry <rhanberry@catalystitservices.com>
2016-01-11 11:20:47 -0800

    undid prevuois commit, dependencies need to be resolved before removing prev. java files

--- src/main/java/com/collectorthrd/config/LoggingAspectConfiguration.java
+++ src/main/java/com/collectorthrd/config/LoggingAspectConfiguration.java
@@ -1,19 +1,19 @@
-//package com.collectorthrd.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.EnableAspectJAutoProxy;
-//import org.springframework.context.annotation.Profile;
-//
-//import com.collectorthrd.aop.logging.LoggingAspect;
-//
-//@Configuration
-//@EnableAspectJAutoProxy
-//public class LoggingAspectConfiguration {
-//
-//    @Bean
-//    @Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
-//    public LoggingAspect loggingAspect() {
-//        return new LoggingAspect();
-//    }
-//}
+package com.collectorthrd.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+
+import com.collectorthrd.aop.logging.LoggingAspect;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
